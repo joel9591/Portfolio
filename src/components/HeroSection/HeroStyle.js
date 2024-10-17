81672f8b047a43f93aedae938ac58e5b58ca9857
Joel9591 <joelstalin76@gmail.com>
2024-10-17 19:01:55 +0530

    Updated portfolio layout and fixed mobile image centering

--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -78,9 +78,9 @@ export const HeroRightContainer = styled.div`
   width: 100%;
   display: flex;
   order: 2;
-  justify-content: end;
+  justify-content: center;
   gap: 12px;
-  margin-left : 130px;
+  margin-left : 0;
 
   @media (max-width: 960px) {
     order: 1;
@@ -97,7 +97,7 @@ export const HeroRightContainer = styled.div`
 export const Img = styled.img`
   position: relative;
   width: 100%;
-  height: 100%;
+  height: auto;
   max-width: 400px;
   max-height: 400px;
   border-radius: 50%;
@@ -105,13 +105,13 @@ export const Img = styled.img`
   transition: transform 0.1s ease-out; /* Smooth transition for rotation and zoom */
 
   @media (max-width: 768px) {
-    max-width: 400px;
-    max-height: 400px;
+    max-width: 300px;
+    max-height: 300px;
   }
 
   @media (max-width: 640px) {
-    max-width: 280px;
-    max-height: 280px;
+    max-width: 200px;
+    max-height: 200px;
   }
 `;
 
